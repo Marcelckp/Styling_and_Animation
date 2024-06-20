ca2df41bb87f81498f13e64e9d7235771a584a50
Marcelckp <81308023+Marcelckp@users.noreply.github.com>
2024-06-20 21:55:34 +0200

    chore: added staggering object example

--- GSAP/Express/examples/gsapStaggering.js
+++ GSAP/Express/examples/gsapStaggering.js
@@ -1,7 +1,24 @@
 gsap.to(".star", {
-  stagger: 1,
+  stagger: 1, // If we less the stagger value we will see the stars movement overlapping
   x: 750,
   rotation: 360,
   fill: "cyan",
+  duration: 3,
+});
+
+
+// Stagger example with stagger object
+gsap.to('.blue-star', {
+  stagger: {
+    // amount: 1,
+    each: 0.5,
+    from: 'center', // "end", "start", "edges"
+    // grid: 'auto',
+    duration: 3,
+    },
+  ease: 'bounce',
+  x: 750,
+  rotation: 360,
+  fill: 'cyan',
   duration: 3,
 });
